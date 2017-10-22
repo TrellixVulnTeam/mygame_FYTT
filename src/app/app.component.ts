0f5a001d81cab4750999bcfe54c9ac468653eb5e
Danny Jordan <dannyjordanmacbook@dannys-mbp-2.home>
2017-10-22 20:30:54 +0100

    my game app com 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `<h1>Welcome to the {{name}}</h1>
+
+  	<h2>Q1, What year did Arrested Development has a hit with Mr Wendal?</h2>
+
+  `,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  { name = 'Live Game Show'; }
