161fae3896d19535c50e14dd3d608e2b476530db
Danny Jordan <danny@amershamwebsites.co.uk>
2017-10-23 18:50:55 +0100

    first commit

---
+++ mygame-app/src/app/app.component.ts
@@ -0,0 +1,51 @@
+import { Component } from '@angular/core';
+
+export class Question {
+	questionNumber: number;
+	questionText: string;
+	answers: any;	
+}
+
+export class Answer {
+	answerid: string;
+	answerText: string;	
+}
+
+const QUESTIONS: Question[] = [
+	{
+		questionNumber: 1,
+	  	questionText: 'What is the highest selling record of all time?',
+		answers: [
+			{
+				answerid: 'Q1A1',
+				answerText: 'Answer 1'	
+			},
+			{
+				answerid: 'Q1A2',
+				answerText: 'Answer 2'	
+			},
+			{
+				answerid: 'Q1A3',
+				answerText: 'Answer 3'	
+			},
+			{
+				answerid: 'Q1A4',
+				answerText: 'Answer 4'	
+			}
+		]
+	}
+];
+
+@Component({
+  selector: 'mygame',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+ 	title = 'My Game Show';
+ 	questions = QUESTIONS;	
+}
+
+
+
